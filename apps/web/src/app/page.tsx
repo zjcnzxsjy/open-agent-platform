447d0fb2eb1fe38a8bdca0946d84fe7e0c78a131
bracesproul <braceasproul@gmail.com>
2025-04-15 17:52:02 -0700

    Initial commit

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Toaster } from "@/components/ui/sonner";
+import React from "react";
+
+export default function DemoPage(): React.ReactNode {
+  return (
+    <React.Suspense fallback={<div>Loading (layout)...</div>}>
+      <Toaster />
+    </React.Suspense>
+  );
+}
