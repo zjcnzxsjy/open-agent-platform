abfd0b5fac1135de00303e98680c338047def687
bracesproul <braceasproul@gmail.com>
2025-05-12 16:50:20 -0700

    chore: Update env example

--- apps/web/src/lib/environment/tools.ts
+++
@@ -1,9 +0,0 @@
-import { Tool } from "@/types/tool";
-
-/**
- * Loads the provided tools from the environment variable.
- * @returns {Tool[]} The list of tools.
- */
-export function getTools(): Tool[] {
-  return JSON.parse(process.env.NEXT_PUBLIC_TOOLS || "[]");
-}
