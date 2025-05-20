d3707b1e74d0610345f116109506cb689786dd1a
bracesproul <braceasproul@gmail.com>
2025-05-20 10:27:48 -0700

    fix: Calling MCP servers without auth

--- apps/web/src/hooks/use-mcp.tsx
+++ apps/web/src/hooks/use-mcp.tsx
@@ -4,23 +4,12 @@ import { Tool } from "@/types/tool";
 import { useState } from "react";
 
 function getMCPUrlOrThrow() {
-  if (!process.env.NEXT_PUBLIC_MCP_SERVER_URL) {
-    throw new Error("NEXT_PUBLIC_MCP_SERVER_URL is not defined");
-  }
-
-  if (process.env.NEXT_PUBLIC_MCP_AUTH_REQUIRED !== "true") {
-    // Do not use proxy route, and use the URL directly
-    const mcpUrl = new URL(process.env.NEXT_PUBLIC_MCP_SERVER_URL);
-    mcpUrl.pathname = `${mcpUrl.pathname}/mcp`;
-    return mcpUrl;
-  }
-
   if (!process.env.NEXT_PUBLIC_BASE_API_URL) {
     throw new Error("NEXT_PUBLIC_BASE_API_URL is not defined");
   }
 
   const url = new URL(process.env.NEXT_PUBLIC_BASE_API_URL);
-  url.pathname = `${url.pathname}/oap_mcp`;
+  url.pathname = `${url.pathname}${url.pathname.endsWith("/") ? "" : "/"}oap_mcp`;
   return url;
 }
 
@@ -37,6 +26,7 @@ export default function useMCP({
 }) {
   const [tools, setTools] = useState<Tool[]>([]);
   const [cursor, setCursor] = useState("");
+
   /**
    * Creates an MCP client and connects it to the specified server URL.
    * @param url - The URL of the MCP server.
@@ -47,7 +37,7 @@ export default function useMCP({
    */
   const createAndConnectMCPClient = async () => {
     const url = getMCPUrlOrThrow();
-    const connectionClient = new StreamableHTTPClientTransport(url);
+    const connectionClient = new StreamableHTTPClientTransport(new URL(url));
     const mcp = new Client({
       name,
       version,
